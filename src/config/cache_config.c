0ce1a09569efafdb8179318fa32e782a73ea5cff
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-05-03 14:16:43 -0600

    basic setup

---
+++ src/config/cache_config.c
@@ -0,0 +1,44 @@
+//Copyright 2021 Stratify Labs, See LICENSE.md for details
+
+#include "config.h"
+
+#define USE_CACHE 0
+
+void cache_enable() {
+#if USE_CACHE
+  SCB_EnableICache();
+  SCB_EnableDCache();
+#endif
+}
+
+void cache_disable() {
+#if USE_CACHE
+  SCB_DisableICache();
+  SCB_DisableDCache();
+#endif
+}
+void cache_invalidate_instruction() {
+#if USE_CACHE
+  SCB_InvalidateICache();
+#endif
+}
+void cache_invalidate_data() {
+#if USE_CACHE
+  SCB_InvalidateDCache();
+#endif
+}
+void cache_invalidate_data_block(void *address, size_t size) {
+#if USE_CACHE
+  SCB_InvalidateDCache_by_Addr(address, size);
+#endif
+}
+void cache_clean_data() {
+#if USE_CACHE
+    SCB_CleanDCache();
+#endif
+}
+void cache_clean_data_block(void *address, size_t size) {
+#if USE_CACHE
+  SCB_CleanDCache_by_Addr(addr, size);
+#endif
+}
---
+++ src/config/os_config.c
@@ -0,0 +1,26 @@
+
+//Copyright 2021 Stratify Labs, See LICENSE.md for details
+
+#include "os_config.h"
+
+void os_event_handler(int event, void *args) {
+
+  switch (event) {
+
+  case SOS_EVENT_ROOT_FATAL:
+    // start the bootloader on a fatal event
+    // mcu_core_invokebootloader(0, 0);
+    if (args != 0) {
+      sos_debug_printf("Fatal Error %s\n", (const char *)args);
+    } else {
+      sos_debug_printf("Fatal Error unknown\n");
+    }
+    cortexm_reset(NULL);
+    break;
+
+  case SOS_EVENT_START_LINK:
+    break;
+
+  }
+}
+
